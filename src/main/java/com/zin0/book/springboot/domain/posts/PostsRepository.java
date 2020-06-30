2308c87ff17e318d2c5872d94a2337b18d776972
dimpl@JinYoung <dimple0416@naver.com>
2020-06-30 23:00:27 +0900

    upload : up to chapter4, makes some views by mustache

--- src/main/java/com/zin0/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/zin0/book/springboot/domain/posts/PostsRepository.java
@@ -1,6 +1,11 @@
 package com.zin0.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {   // JpaRepository<Entity 클래스, PK 타입> 상속 ~> CRUD 자동 생성
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/zin0/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/zin0/book/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.zin0.book.springboot.service.posts;
 
 import com.zin0.book.springboot.domain.posts.Posts;
 import com.zin0.book.springboot.domain.posts.PostsRepository;
+import com.zin0.book.springboot.web.dto.PostsListResponseDto;
 import com.zin0.book.springboot.web.dto.PostsResponseDto;
 import com.zin0.book.springboot.web.dto.PostsSaveRequestDto;
 import com.zin0.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -24,12 +27,22 @@ public class PostsService {
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
         Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id =" + id));
         posts.update(requestDto.getTitle(), requestDto.getContent());
-
         return id;
     }
 
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id =" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true) // readOnly에 error 뜨길래 뭐지하고 찾아보니까, import에 javax.Transactional 이 임포트 돼있었음.. springframework.transaction~ 이다..
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id =" + id));
+        postsRepository.delete(posts);
     }
 }
