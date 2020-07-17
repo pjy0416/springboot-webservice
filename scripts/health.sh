8e0dc1bddd4034a2ec3b3608f583352fcd7aa353
dimpl@JinYoung <dimple0416@naver.com>
2020-07-17 18:17:34 +0900

    modify : modify shell scripts

--- scripts/health.sh
+++ scripts/health.sh
@@ -2,28 +2,28 @@
 
 ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
-source=${ABSDIR}/profile.sh
-source=${ABSDIR}/switch.sh
+source ${ABSDIR}/profile.sh
+source ${ABSDIR}/switch.sh
 
 IDLE_PORT=$(find_idle_port)
 
 echo "> Health Check Start!"
 echo "> IDLE_PORT: $IDLE_PORT"
-echo "> curl -s http://localhost:$IDLE_PORT/profile"
+echo "> curl -s http://localhost:$IDLE_PORT/profile "
 sleep 10
 
-for RETRY_COUNT in {1..0}
+for RETRY_COUNT in {1..10}
 do
   RESPONSE=$(curl -s http://localhost:${IDLE_PORT}/profile)
-  UP_COUNT=$(echo ${RESPONSE} | grep 'real' | wc -1)
+  UP_COUNT=$(echo ${RESPONSE} | grep 'real' | wc -l)
 
   if [ ${UP_COUNT} -ge 1 ]
-  then # $up_count >=1 ("real" 문자열이 있는지 검증)
+  then # $up_count >= 1 ("real" 문자열이 있는지 검증)
       echo "> Health check 성공"
       switch_proxy
       break
   else
-      echo "> Helath check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
+      echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
       echo "> Health check: ${RESPONSE}"
   fi
 
@@ -34,6 +34,6 @@ do
     exit 1
   fi
 
-  echo "> Health check 연결 실패. 패시도..."
+  echo "> Health check 연결 실패. 재시도..."
   sleep 10
 done
--- scripts/start.sh
+++ scripts/start.sh
@@ -2,20 +2,20 @@
 
 ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
-source=${ABSDIR}/profile.sh
+source ${ABSDIR}/profile.sh
 
 REPOSITORY=/home/ec2-user/app/step3
-PROJECT_NAME=zin0Study-springboot2-webservice # 에러뜨면 여기부터 확인하기
+PROJECT_NAME=zin0Stduy-springboot2-webservice
 
 echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-echo "> 새 애플리케이션 배포"
-JAR_NAME=$(ls -tr $REPOSITORY/*jar | tail -n 1)
+echo "> 새 어플리케이션 배포"
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
-echo "> JAR NAME: $JAR_NAME"
+echo "> JAR Name: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
 
